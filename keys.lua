55b8c7f5d9f4d8a10a6b5428346080023221ad02
Andy Kreling <krelinga@gmail.com>
2023-10-01 19:23:37 -0500

    easy key bind to open the file explorer.

--- keys.lua
+++ keys.lua
@@ -2,3 +2,6 @@ local map = vim.api.nvim_set_keymap
 
 -- remap the key used to leave insert mode
 map('i', 'jk', '<esc>', {})
+
+-- Open the nvim file tree with a key bind.
+map('n', 'n', [[:NvimTreeToggle<CR>]], {})
